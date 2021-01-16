78d12ca337ab257cf7e36c98c4428b8a9155525c
Carlos Mtz <isc.carlosmtz@gmail.com>
2021-01-16 06:16:36 +0000

    Actualizado configuracion para el sitio

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,12 +10,59 @@ module.exports = {
     siteUrl: 'https://carlosmtz.dev/'
   },
   plugins: [
-    'gatsby-plugin-cname',
+    `gatsby-plugin-cname`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-remark`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-67858265-1",
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`400`, `700`]
+          }
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-i18n',
+      options: {
+        //.. other options
+        pagesPaths: [ `${__dirname}/src/posts`],
+        langKeyDefault: 'es',
+        prefixDefault: false,
+        useLangKeyLayout: false,
+        markdownRemark: {
+          postPage: 'src/templates/blogTemplate.js',
+          query: `
+            {
+              allMarkdownRemark {
+                edges {
+                  node {
+                    fields {
+                      slug,
+                      langKey
+                    }
+                  }
+                }
+              }
+            }
+          `
+        }
+      }
+    }
   ],
 }
